3b0ba8760fe3083093502cc202e3ece213c7f937
Strée Eloi <jams.center@gmail.com>
2019-04-07 04:07:21 +0100

    Add Morse Detector and several tools used in other projects

---
+++ Assets/MorseDetector/Script/Value/MorseValueWithOrigine.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct MorseValueWithOrigine : MorseValueInterface {
+
+    public MorseValueWithOrigine(string emittorName, params MorseKey[] keys): this(emittorName, new MorseValue(keys))
+    {}
+    public MorseValueWithOrigine(string emittorName, MorseValue value) {
+        m_emittorName = emittorName;
+        m_morse = value;
+    }
+
+    [SerializeField]
+    MorseValue m_morse;
+    public MorseValue GetMorseValue() { return m_morse;}
+
+    [SerializeField]
+    string m_emittorName;
+    public string GetEmittorName() {
+        return m_emittorName;
+    }
+    public MorseKey[] GetValue()
+    {
+        return m_morse.GetValue();
+    }
+}
